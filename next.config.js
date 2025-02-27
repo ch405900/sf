90dc5fe77a409f20c2dbf2140c01d932aac2ebfc
ch405900 <ch405900@163.com>
2025-02-27 12:16:30 +0800

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     eslint: {
         ignoreDuringBuilds: true,
     },
@@ -8,12 +9,6 @@ const nextConfig = {
         tsconfigPath: './tsconfig.json'
     },
     staticPageGenerationTimeout: 1800,
-    i18n: {
-        locales: ['en', 'zh'], // 只保留需要的语言
-        defaultLocale: 'en'
-    },
-    // 阻止自动检测语言（减少预生成路径）
-    localeDetection: false,
     experimental: {
         granularChunks: {
             i18n: 'lazy' // 延迟加载语言包
